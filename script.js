b37520fb022b969f3f5546482431b6215280ee12
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-22 09:51:46 -0500

    Refactored ID method and completed functionality

--- script.js
+++ script.js
@@ -25,31 +25,73 @@ class Book {
 function newBook(event){
     event.preventDefault();
     const formData = new FormData(inputForm);
-    library.push(new Book(
+    let checked = false;
+    if(formData.get("read")){
+        checked = true;
+    }
+    let newIndex = library.push(new Book(
         formData.get("author"),
         formData.get("title"),
         formData.get("pages"),
-        formData.get("read")
-    ));
+        checked
+    )) - 1;
+
+    console.log(newIndex);
+    console.log(library[newIndex]);
 
     inputDialog.close();
+
+    const newBookCard = document.createElement("div");
+    newBookCard.classList += "book-card";
+    newBookCard.id = newIndex;
+
+    const newSTitle = document.createElement("span");
+    newSTitle.innerText = "Title: " + library[newIndex].title;
+    newBookCard.appendChild(newSTitle);
+
+    const newSAuthor = document.createElement("span");
+    newSAuthor.innerText = "Author: " + library[newIndex].author;
+    newBookCard.appendChild(newSAuthor);
+
+    const newSPages = document.createElement("span");
+    newSPages.innerText = "Pages: " + library[newIndex].pages;
+    newBookCard.appendChild(newSPages);
+
+    const newSRead = document.createElement("span");
+
+    newSRead.innerText = "Have you read this: "
+    const newInput = document.createElement("input");
+    newInput.type = "checkbox";
+    newInput.name = "read-checkbox";
+    newInput.classList += "read-checkbox";
+    newInput.checked = library[newBookCard.id].read;
+    newInput.addEventListener("click", bookCheckboxClicked);
+    newSRead.appendChild(newInput);
+    newBookCard.appendChild(newSRead);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList += "delete-button";
+    deleteButton.innerText = "Remove";
+    deleteButton.addEventListener("click", deleteBook);
+    newBookCard.appendChild(deleteButton);
+
+
+    libraryContainer.appendChild(newBookCard);
+    console.log(library[newIndex].read);
     
-    updateLibraryCards();
 }
 
+function bookCheckboxClicked(event){
+    library[event.target.parentElement.parentElement.id].read = !library[event.target.parentElement.parentElement.id].read;
+}
 
-function updateLibraryCards(){
-    if(library.length > 0){
-        for(i = 0; i<library.length; i++){
-            const matchingBook = document.getElementById(`${i}`);
-            if(!matchingBook){
-                const newBookCard = document.createElement("div");
-                newBookCard.innerText = i;
-                newBookCard.id = i;
-                newBookCard.classList += "book-card";
-                libraryContainer.appendChild(newBookCard);
-                console.log(library[i].author);
-            }
-        }
+function deleteBook(event){
+    library.splice(event.target.parentElement.id, 1);
+    event.target.parentElement.remove();
+    let nodes = libraryContainer.childNodes;
+    for(i = 0; i<nodes.length; i++){
+        nodes[i].id = i;
     }
+
+    console.log(nodes.length);
 }
